46274be2b516112f046f467e7d7d2856ce707cc2
Merylabu <46573690+Merylabu@users.noreply.github.com>
2019-01-10 20:26:13 +0100

    Add files via upload
    Create proyect

---
+++ EjercicioSatelite/src/main/java/Satelite.java
@@ -0,0 +1,28 @@
+
+public class Satelite {
+	public double meridiano;
+	public double paralelo;
+	public double distancia_tierra;
+
+	public Satelite(double m, double p, double d) {
+		meridiano = m;
+		paralelo = p;
+		distancia_tierra = d;
+	}
+
+	public Satelite() {
+		meridiano = paralelo = distancia_tierra = 0;
+	}
+
+	public void setPosicion(double m, double p, double d) {
+		meridiano = m;
+		paralelo = p;
+		distancia_tierra = d;
+	}
+
+	public void printPosicion() {
+		System.out.println("El satélite se encuentra en el paralelo" + paralelo + "Meridiano" + meridiano
+				+ "a una distancia de la tierra de" + distancia_tierra + "Kilómetros");
+		
+	}
+}
